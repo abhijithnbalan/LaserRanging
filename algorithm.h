f3f8aac50780ea7faec05b5ce10ac8833389b7da
pplankton <abhijithnbalan@gmail.com>
2018-01-17 16:53:24 +0530

    version 2.2

--- algorithm.h
+++ algorithm.h
@@ -9,7 +9,7 @@ class Algorithm
   public:
     CaptureFrame CLAHE_dehaze(CaptureFrame ); //CLAHE based basic dehazing algorithm
     cv::Mat CLAHE_dehaze(cv::Mat); //CLAHE based basic dehazing algorithm
-
+    CaptureFrame hist_equalize(CaptureFrame);
 };
 
 #endif
